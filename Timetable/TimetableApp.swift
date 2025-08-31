da9e507e5583a68054367e9d1afe7cae0367fb15
董承威 <109213867+AndyTung401@users.noreply.github.com>
2025-08-31 16:45:43 +0800

    Initial Commit

---
+++ Timetable/TimetableApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimetableApp.swift
+//  Timetable
+//
+//  Created by 董承威 on 2025/8/31.
+//
+
+import SwiftUI
+
+@main
+struct TimetableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
